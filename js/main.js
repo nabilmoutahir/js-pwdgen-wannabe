5152352ea4648fe00c4e5cd2f7ac41bbafd43bf9
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-11 19:48:57 +0100

    feat: cognome

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 // Chiedo il Nome
 const UserFirstName = prompt("Qual è il tuo Nome?");
 console.log(UserFirstName);
+
+// Chiedo il Cognome
+const UserLastName = prompt("Qual è il tuo Cognome?");
+console.log(UserLastName);
